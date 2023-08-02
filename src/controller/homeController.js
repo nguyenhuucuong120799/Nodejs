90f3b608e27e72f62362897f22521bd196088104
CC9x <66666906+nguyenhuucuong120799@users.noreply.github.com>
2023-08-02 15:59:40 +0700

    Co-authored-by: nguyenhuyanh2222@gmail.com <nguyenhuyanh2222@gmail.com>

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,23 +2,23 @@ import connection from '../configs/connectDB';
 
 let getHomepage = (req, res) => {
     //logic
-    let statement = 'SELECT * FROM users';
-    let datas = [];
-    connection.query(statement, (err, results) => {
-        if(results){
-            console.log(results); // results contains rows returned by server
-            datas = results.map((element) => { return element });
-            console.log(datas.TextRow[0]); 
-        }
-        else if(err){
-            console.log(err);
-        }
-    });
-    console.log(datas);
-    return res.render('test/index.ejs', { user: JSON.stringify(datas) });
-    // return res;
-}
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    Id: row.Id,
+                    email: row.email,
+                    address: row.address,
+                    Firstname: row.Firstname,
+                    Lastname: row.Lastname  
+})
+            });
+            return res.render('index.ejs', {dataUser: data})
+        })
+    }
 
 module.exports = {
     getHomepage
-}
+} 
